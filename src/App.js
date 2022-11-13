f46b247c5c43ad7a0e4542d2f3992dcc2ee97fb0
Pranit Bhegade <111244937+Pranit-DB@users.noreply.github.com>
2022-11-13 15:08:21 +0530

    Update App.js
    cleaning up code

--- src/App.js
+++ src/App.js
@@ -8,147 +8,6 @@ import SignIn from './components/SignIn/SignIn';
 import Register from './components/Register/Register';
 import Rank from './components/Rank/Rank';
 import './App.css';
-// import { ClarifaiStub, grpc } from 'clarifai-nodejs-grpc';
-
-// const Clarifai = require( 'clarifai')
-// import Clarifai from 'clarifai'
-// import FaceRecApi from "./components/FaceRecApi/FaceRecApi";
-
-// console.log(Clarifai);
-// const app = new Clarifai.App({
-//   apiKey:'9d0a34e1f8ba4f08b2ea3d16f2bf2c88'
-// });
-
-                        // class App extends Component {
-                        //   constructor(){
-                        //     super();
-                        //     this.state = {
-                        //       input: '',
-                        //       imageUrl:'',
-                        //       box:{},
-
-                        //     };
-                        //   }
-
-  // function App(){
-    
-  //  var calculateFaceLocation = (data)=>{
-  //     const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
-  //     const image = document.getElementById('inputimage');
-  //     const width = Number(image.width);
-  //     const height = Number(image.height);
-  //     console.log(width,height);
-  //     return {
-  //       leftCol: clarifaiFace.left_col * width,
-  //       topRow: clarifaiFace.top_row * height,
-  //       rightCol: width - (clarifaiFace.right_col * width),
-  //       bottomRow: height - (clarifaiFace.bottom_row * height),
-  //     }
-  //   }
-
-  //   var displayFaceBox = (box) =>{
-  //     console.log(box);
-  //     this.setState({box:box})
-  //   }
-    // const [input, setInput] = useState('');
-    // const [imageUrl, setImageUrl] = useState('');
-
-    // useEffect(()=>{
-    //   const getImage = () => {
-    //     const img = new Image();
-    //     img.src = onInputChange();
-    //     img.onload = () => {
-    //       setImageUrl({
-    //         url: img.src,
-    //         width: img.width,
-    //         height: img.height,
-    //       });
-    //     };
-    //   }
-    //   getImage();
-    // },[]);
-
-                        // onInputChange = (event) => {
-                        //   this.setState({input:event.target.value})
-                        // };
-                        // onButtonSubmit = () => {
-                        //   this.setState({imageUrl:this.state.input});
-                        //   console.log(this.state.imageUrl);
-                        // }
-
-    // const onButtonSubmit = () => {
-      // setimageUrl(input);
-      // app.models.predict(
-      //   'a403429f2ddf4b49b307e318f00e528b', // this should work
-      //   // Clarifai.FACE_DETECT_MODEL,  // if this isn't
-      //   // "http://sample.websitesforchildcare.com.au/uploads/images/kids-playing.jpg"
-      //   this.state.input
-      // ).then(response => 
-      //   displayFaceBox(
-      //   calculateFaceLocation(response))
-      //   )
-      //  .catch(err => console.log(err));
-    
-
-                    //     render() {
-                    //       const { imageUrl, onButtonSubmit, onInputChange } = this.state
-                    //        return (
-                    //         <div className="App">
-                    //           <div className="particles">
-                    //             <ParticleBackground /></div>
-                    //           <Navigation />
-                    //           <Logo />
-                    //           <ImageLinkForm
-                    //            onInputChange={onInputChange} 
-                    //            onButtonSubmit={onButtonSubmit}/>
-                    //           {/* <FaceRecognition  imageUrl={imageUrl}/> */}
-                    //           <FaceRecApi 
-                    //           imageUrl={imageUrl} 
-                    //           />
-                    //         </div>
-                    //       );
-                    //     }
-                    //   }
-                    // export default App;
-
-/////////////////////////////////////////////////////////////////////////////////////////
-/////////////////////////////////////////   v2   ////////////////////////////////////////
-/////////////////////////////////////////////////////////////////////////////////////////
-
-// function App () {
-
-//   // const [ isLoading, setLoading ] = useState(false);
-//   const [ input, setInput ] = useState('');
-//   const [ imageUrl, setImageUrl ] = useState('');
-  
-//   const onInputChange = (event) => {
-//     event.preventDefault();
-//     setInput(event.target.value)
-//   };
-//   const onButtonSubmit = () => {
-//     setImageUrl(input);
-//   }
-
-//   return (
-//     <div className="App">
-//       <div className="particles">
-//         <ParticleBackground /></div>
-//       <Navigation />
-//       <Logo />
-//       <ImageLinkForm
-//         onInputChange={ onInputChange }
-//         onButtonSubmit={ onButtonSubmit } 
-//         /> 
-//       <FaceRecognition  imageUrl={imageUrl}/>
-//       {/* <FaceRecApi imageUrl={imageUrl} /> */}
-//     </div>
-//   );
-// }
-// export default App;
-
-/////////////////////////////////////////////////////////////////////////////////////////
-/////////////////////////////////////////   v3   ///////////////////////////////////////
-/////////////////////////////////////////////////////////////////////////////////////////
 
 const initialState = {
   input : '',
@@ -211,15 +70,7 @@ class App extends Component {
 
   onButtonSubmit = () => { 
     this.setState({ imageUrl : this.state.input})
-    // LOADING STATE
-    // if(this.state.input.length === 0){
-    //   this.setState({loading: false})
-    // }else{
-    //   this.setState({loading: true})
-    // }
 
-    //TODO: fetch imgurl from server
-    // fetch(`http://localhost :3000/profile/${this.state.user.id}/${this.state.imageUrl}`,{
       return  fetch('http://localhost:3000/profile/:id/imageUrl',{
       method: 'post',
       headers: {'Content-Type':'application/json'},
@@ -242,18 +93,7 @@ class App extends Component {
         }
         this.displayFaceBox(this.calculateFaceLocation(response))
       }).catch(err=>console.log('outside',err))
-
-    // ............. end of update..............
-    // app.models.predict(
-    //     // 'a403429f2ddf4b49b307e318f00e528b', // this should work
-    //     Clarifai.FACE_DETECT_MODEL,  //   if this isn't
-    //     this.state.imageUrl
-    //   ).then(response =>
-    //     this.displayFaceBox(
-    //     this.calculateFaceLocation(response))
-    //     )
-    //    .catch(err => console.log(err));
-  }
+}
 
   onRouteChange = ( route ) => {
     if( route === 'SignOut'){
